b87de57caa34678a5a90c8290424c7fd941c2f98
hcthanhhh <hcthanh@apcs.vn>
2018-12-23 12:18:49 +0700

    byQuang

---
+++ MEVO/mevo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mevo.MvcApplication" Language="C#" %>
---
+++ MEVO/mevo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mevo.Startup))]
+namespace mevo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
