1fd137d5a36506d6e72b060b3645f7b403acfbab
Minh Anh <ntmanh@apcs.vn>
2018-12-29 19:55:56 +0700

    message

---
+++ MEVO/message.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#action_menu_btn').click(function(){
+        $('.action_menu').toggle();
+    });
+});
+/*
+$('#btnClick').on('click',function(){
+    if($('#contact1').css('display')!='none'){
+        $('#2').html('Here is my dynamic content').show().siblings('div').hide();
+    }else if($('#2').css('display')!='none'){
+        $('#1').show().siblings('div').hide();
+    }
+});*/
